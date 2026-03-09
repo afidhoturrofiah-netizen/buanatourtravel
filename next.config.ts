4f2be11c8e73c79327b5a953187a6a686a9f7776
irvansindu <irvansindu653@gmail.com>
2026-03-09 09:47:06 +0700

    Add Vercel Blob domain to Next.js Image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   reactCompiler: true,
   outputFileTracingRoot: path.join(process.cwd()),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
